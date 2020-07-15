4ad165e0da61b00a4d3c982c7351bd619e0cc34c
take <n2l2s2983@gmail.com>
2020-07-15 19:57:21 +0000

    index fixed

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,9 +1,10 @@
 class TasksController < ApplicationController
   before_action :require_user_logged_in
   before_action :set_tasks, only: [:show,:edit, :update, :destroy]
+  before_action :correct_user, only: [:show,:edit, :update, :destroy]
 
   def index
-    @tasks = Task.order(id: :desc).page(params[:page])
+    @tasks = current_user.tasks.order(id: :desc).page(params[:page])
   end
 
   def show
@@ -14,11 +15,11 @@ class TasksController < ApplicationController
   end
 
   def create
-    @task = Task.new(task_params)
+    @task = current_user.tasks.build(task_params)
     
     if @task.save
       flash[:success]= "Task が正常に登録されました"
-      redirect_to @task
+      redirect_to ("/")
     else
       flash.now[:danger]= "Taskが登録されませんでした"
       render :new
@@ -56,4 +57,10 @@ class TasksController < ApplicationController
     params.require(:task).permit(:content, :status)
   end
   
+  def correct_user
+  @micropost = current_user.tasks.find_by(id: params[:id])
+    unless @micropost
+      redirect_to root_url
+    end
+  end  
 end
