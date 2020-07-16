897f4bcd10c56d90d6d52bb619a82148bd418514
take <n2l2s2983@gmail.com>
2020-07-16 13:18:29 +0000

    routes_fix

--- config/routes.rb
+++ config/routes.rb
@@ -4,12 +4,7 @@ Rails.application.routes.draw do
   get "login", to: "sessions#new"
   post "login", to: "sessions#create"
   delete "logout", to: "sessions#destroy"
-  
-  get 'users/new'
-  get 'users/create'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-    
   get "signup", to: "users#new"
   post "users", to: "users#create"
   resources :tasks
