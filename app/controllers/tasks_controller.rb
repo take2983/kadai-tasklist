a470c619293f6a6d6cefea428c3c49c3352116c1
take <n2l2s2983@gmail.com>
2020-07-16 12:27:08 +0000

    fix

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,5 @@
 class TasksController < ApplicationController
   before_action :require_user_logged_in
-  before_action :set_tasks, only: [:show,:edit, :update, :destroy]
   before_action :correct_user, only: [:show,:edit, :update, :destroy]
 
   def index
@@ -49,17 +48,14 @@ class TasksController < ApplicationController
   private
   
   #Strong Parameter
-  def set_tasks
-    @task = Task.find(params[:id])
-  end
   
   def task_params
     params.require(:task).permit(:content, :status)
   end
   
   def correct_user
-  @micropost = current_user.tasks.find_by(id: params[:id])
-    unless @micropost
+  @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
       redirect_to root_url
     end
   end  
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,6 @@
 Rails.application.routes.draw do
   root to: "tasks#index"
-  
-  get 'sessions/new'
-  get 'sessions/create'
-  get 'sessions/destroy'
-  
-    
+ 
   get "login", to: "sessions#new"
   post "login", to: "sessions#create"
   delete "logout", to: "sessions#destroy"
